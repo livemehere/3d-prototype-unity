d148d08991a9c081a6a56f52e20566b57776adbc
livemehere <rhdxoals@gmail.com>
2026-07-01 03:20:47 +0900

    enemy knockback

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,14 +3,27 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float lifetime = 3f;
+    public float knockbackForce = 1f;
 
     void Start()
     {
         Destroy(gameObject, lifetime);
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var em = other.gameObject.GetComponentInParent<EnemyMovement>();
+            if (em)
+            {
+                Vector3 knockbackDir = transform.forward;
+                knockbackDir.y = 0;
+                knockbackDir.Normalize();
+                em.Knockback(knockbackDir, knockbackForce);
+            }
+        }
+
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
+    [SerializeField] private float knockbackDamping = 8f;
+    [SerializeField] private float minKnockbackSpeed = 0.05f;
+
     private NavMeshAgent agent;
-    
+    private Vector3 knockbackVelocity;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        
     }
- 
+
     // Update is called once per frame
     void Update()
     {
@@ -20,6 +23,24 @@ public class EnemyMovement : MonoBehaviour
         {
             agent.SetDestination(player.position);
         }
-        
+
+        if (knockbackVelocity.sqrMagnitude > minKnockbackSpeed * minKnockbackSpeed)
+        {
+            agent.Move(knockbackVelocity * Time.deltaTime);
+            knockbackVelocity = Vector3.Lerp(
+                knockbackVelocity,
+                Vector3.zero,
+                knockbackDamping * Time.deltaTime
+            );
+        }
+        else
+        {
+            knockbackVelocity = Vector3.zero;
+        }
+    }
+
+    public void Knockback(Vector3 dir, float force)
+    {
+        knockbackVelocity = dir.normalized * force;
     }
 }
