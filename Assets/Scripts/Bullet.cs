21c94fcae84c7d1d3d8f9d70f64b62f23f82c62c
livemehere <rhdxoals@gmail.com>
2026-07-02 02:17:44 +0900

    enemy has hp

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,28 +2,42 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public int damage = 1;
     public float lifetime = 3f;
     public float knockbackForce = 1f;
+    public GameObject particle;
 
     void Start()
     {
         Destroy(gameObject, lifetime);
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy"))
         {
-            var em = other.gameObject.GetComponentInParent<EnemyMovement>();
+            var em = collision.gameObject.GetComponentInParent<EnemyMovement>();
             if (em)
             {
                 Vector3 knockbackDir = transform.forward;
                 knockbackDir.y = 0;
                 knockbackDir.Normalize();
                 em.Knockback(knockbackDir, knockbackForce);
+                Heath hp = collision.gameObject.GetComponentInParent<Heath>();
+                if (hp)
+                {
+                    hp.TakeDamage(damage);
+                }
             }
         }
 
+        CreateParticle(collision.contacts[0]);
         Destroy(gameObject);
+    }
+
+    void CreateParticle(ContactPoint contact)
+    {
+        var rotation = Quaternion.LookRotation(contact.normal);
+        Instantiate(particle, contact.point, rotation);
     }
 }
---
+++ Assets/Scripts/Heath.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Heath : MonoBehaviour
+{
+    public int hp = 3;
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
